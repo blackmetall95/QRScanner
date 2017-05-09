4eeb016a5fb583d136b64d6ac94975901789d85f
blackmetall95 <rzcc5911@yahoo.co.uk>
2017-05-09 17:37:53 +0800

    Fragment (Complete)
    +Fragment implemented
    +FAB button fixed
    -ListView array not implemented
    -FAB functionality not implemented

--- app/src/main/java/com/jiajie/qrscanner/FragList.java
+++ app/src/main/java/com/jiajie/qrscanner/FragList.java
@@ -5,11 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-public class ListFragment extends Fragment {
+public class FragList extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.list_view,container, false);
+        return inflater.inflate(R.layout.list_frag, container, false);
+
     }
+
+    public FragList(){
+    }
+
 }
--- app/src/main/java/com/jiajie/qrscanner/MainActivity.java
+++ app/src/main/java/com/jiajie/qrscanner/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jiajie.qrscanner;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.*;
+import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +16,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +36,22 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        lv = (ListView) findViewById(R.id.list);
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Alpha");
+        arrayList.add("Bravo");
+        arrayList.add("Charlie");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_frag, arrayList);
+        lv.setAdapter(arrayAdapter);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  //      .setAction("Action", null).show();
+
             }
         });
 
