7cd67d24af4fb47c9cb8a3174ffa7274f8929c20
blackmetall95 <rzcc5911@yahoo.co.uk>
2017-05-25 11:07:13 +0800

    Revert "LocationManager (Complete)"
    This reverts commit 0b107e54b114699e58fce57be8166142a81f1213.

--- app/src/main/java/com/jiajie/qrscanner/GPSLocation.java
+++ app/src/main/java/com/jiajie/qrscanner/GPSLocation.java
@@ -41,7 +41,6 @@ public class GPSLocation implements LocationListener {
 
             if (!isGPSEnabled && !isNetworkEnabled){
                 //no network provider is enabled.
-                ShowSettingsAlert();
             } else{
                 this.canGetLocation = true;
                 if (isGPSEnabled){
